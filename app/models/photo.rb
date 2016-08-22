278b3c4e00656d99d2260b3b2fd4c000001c6539
Marylou Lenhart <melancholyfleur@gmail.com>
2016-08-22 02:10:36 -0400

    Adds 60 photo limit validation
    Since this validation basically requires an association to an album,
    I put a pending status on a test for that requirement for now (as it
    was erroring out due to the dependency).

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,8 +1,19 @@
 class Photo < ActiveRecord::Base
   belongs_to :album
   validates :album, presence: true
+  validate :photo_limit, :on => :create
   validates :name, presence: true
   validates :url, presence: true
   validates_format_of :url, with: /.*jpe*g/i
   paginates_per 10
+
+  def self.photo_limit
+    60
+  end
+
+  def photo_limit
+    if self.album.photos(:reload).count >= Photo.photo_limit
+      errors.add(:base, "You have exceeded the maximum allowed photos for this album")
+    end
+  end
 end
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -18,6 +18,23 @@ RSpec.describe Photo, type: :model do
   end
 
   it "requires an album association" do
+    pending "Not sure how to test this now that another validation basically depends on this one; Maybe raise a validation error in that method?"
     expect(Photo.new(name: "Great Photo Name", url: "http://example.com/photo.jpg")).to_not be_valid
+  end
+
+  it "limits the amount of photos associated with an album" do
+    60.times do
+      @album.photos.create(
+        name: Faker::Lorem.word.capitalize,
+        description: Faker::Lorem.sentence,
+        url: Faker::Avatar.image(SecureRandom.hex, '50x50', 'jpg'),
+        taken_at: Time.now - rand(100).days
+      )
+    end
+    expect(@album.photos.create(name: 'Sad Photo', 
+                        description: "This photo won't be added",
+                        url: "http://www.example.com/sad_photo.jpg",
+                        taken_at: Time.now - rand(100).days
+                       )).to_not be_valid
   end
 end
