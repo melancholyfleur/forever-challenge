cc5961be6b728c68b978f2bc07c4f424ff69ab4b
Marylou Lenhart <melancholyfleur@gmail.com>
2016-08-22 01:04:38 -0400

    Adds Photo validations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,8 @@
 class Photo < ActiveRecord::Base
   belongs_to :album
+  validates :album, presence: true
+  validates :name, presence: true
+  validates :url, presence: true
+  validates_format_of :url, with: /.*jpe*g/i
+  paginates_per 10
 end
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Photo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @album = Album.create(name: "Album 3001")
+  end
+
+  it "requires a name" do
+    expect(Photo.new(name: "", url: "http://example.com/photo.jpg", album: @album)).to_not be_valid
+  end
+
+  it "requires a url" do
+    expect(Photo.new(name: "Great Photo Name", url: "", album: @album)).to_not be_valid
+  end
+
+  it "requires a url end with jpg or jpeg" do
+    expect(Photo.new(name: "Great Photo Name", url: "http://placekitten.com/40/50", album: @album)).to_not be_valid
+  end
+
+  it "requires an album association" do
+    expect(Photo.new(name: "Great Photo Name", url: "http://example.com/photo.jpg")).to_not be_valid
+  end
 end
