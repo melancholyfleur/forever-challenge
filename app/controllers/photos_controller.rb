b9a8428fe20b98e0f9a6acbc43900a02704a7ce1
Marylou Lenhart <melancholyfleur@gmail.com>
2016-08-22 01:04:50 -0400

    Adds Photo index action

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,19 @@
 class PhotosController < ApplicationController
+  def index
+    @photos = Photo.order(:name).page params[:page]
+    @total = Photo.count
+    render json: {photos: @photos, total: @total}, status: :ok, layout: false
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe PhotosController, type: :controller do
+  describe "index" do
+    before(:each) do
+      album = Album.create(name: Faker::Lorem.word.capitalize, position: 1)
+      20.times do |index|
+        album.photos.create(
+          name: Faker::Lorem.word.capitalize,
+          description: Faker::Lorem.sentence,
+          url: Faker::Avatar.image(SecureRandom.hex, '50x50', 'jpg'),
+          taken_at: Time.now - rand(100).days
+        )
+      end
+    end
 
+    it "lists a max of 10 albums per page" do
+      get :index, page: 1
+      expect(JSON.parse(response.body)['photos'].count).to eq 10
+    end
+
+    it "shows next page of albums upon request" do
+      get :index, page: 2
+      expect(response.body).to_not be nil
+      expect(JSON.parse(response.body)['photos'].count).to eq 10
+    end
+
+    it "shows the total number of albums" do
+      get :index, page: 2
+      expect(JSON.parse(response.body)['total']).to eq 20
+    end
+  end
 end
